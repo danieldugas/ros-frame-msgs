68be4e5f7b87a777f2a26651b0fe6501f87a9689
daniel <exodaniel@gmail.com>
2021-01-29 16:26:05 +0100

    adds spencer_tracking_msgs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="ros-frame-msgs",
     description='frame_msgs package from ROS, in pip form',
     author='Daniel Dugas',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
 )
 
